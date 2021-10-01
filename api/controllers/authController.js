cdd1f15ce6eddf93e93d9e1b6fe4de142489503f
Gilmar Oliveira <giilmaroliveira@outlook.com>
2021-10-01 14:46:42 -0300

    Adicionando endpoint para renovacao do token

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -2,38 +2,53 @@ const services = require('../../services');
 const usersServices = services.usersService;
 const bcrypt = require('../../crosscuting/bcrypt');
 const jwt = require('jsonwebtoken');
+const utilResponse = require('../../crosscuting/response');
 
-const auth = async function(req, res, next){
-    let user = await usersServices.authentication(req.body.email);
+const auth = async function (req, res, next) {
+  let user = await usersServices.authentication(req.body.email);
 
-    if(user){
-        let isEqual = await bcrypt.comparePassword(req.body.password, user.password);
+  if (user) {
+    let isEqual = await bcrypt.comparePassword(req.body.password, user.password);
 
-        if(isEqual){
+    if (isEqual) {
 
-            // DTOPattern
-            let userDto = {
-                id: user._id,
-                name: user.name,
-                lastname: user.lastname,
-                fullname: user.name + ' ' + user.lastname,
-                email: user.email,
-                profile: user.profile
-            }
+      // DTOPattern
+      let userDto = {
+        id: user._id,
+        name: user.name,
+        lastname: user.lastname,
+        fullname: user.name + ' ' + user.lastname,
+        email: user.email,
+        profile: user.profile
+      }
 
-            const token = jwt.sign({user: userDto}, "luizaopalmeirensenato", {
-                expiresIn: 6000 // 10 minutos 
-            });
+      const token = jwt.sign({ user: userDto }, "luizaopalmeirensenato", {
+        expiresIn: 6000 // 10 minutos 
+      });
 
-            res.status(200).send({auth: isEqual, user: userDto, token: token});
-        }  
-    } 
+      return res.status(200).send({ auth: isEqual, user: userDto, token: token });
+    }
+  }
 
-    //res.statusCode = 200;
-    res.status(200).send({auth: false, user: null});
+  //res.statusCode = 200;
+  res.status(200).send({ auth: false, user: null });
 
 }
 
+const renew = async function (req, res, next) {
+  try {
+    const token = jwt.sign({ id: req.userJwt.id, profile: req.userJwt.profile }, "luizaopalmeirensenato", {
+      expiresIn: 600 // 10 minutos
+    });
+
+    return res.status(200).send(({ auth: true, token: token }));
+  } catch (error) {
+    res.statusCode = error.errors ? 400 : 500;
+    res.send(utilResponse.format(error));
+  }
+}
+
 module.exports = {
-    auth
+  auth,
+  renew
 }
--- api/routes/authRouter.js
+++ api/routes/authRouter.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const authController = require("../controllers/authController");
+const { verifyJWT } = require("../../middleware/jwtVerify");
 
 router.post('', authController.auth);
+router.post('/renew', verifyJWT, authController.renew);
 
-module.exports = router; 
+module.exports = router;
